85ac2874d1ec4da2e19f5bb7872c9ee01f73dc59
Akash N P <akash.np@ahanait.com>
2024-05-21 19:58:44 +0530

    added edit fms step 1 api

--- routes/initializeFms/initialiseFms.js
+++ routes/initializeFms/initialiseFms.js
@@ -100,8 +100,94 @@ initialiseFms.post('/fmsStep1', async (req, res) => {
         console.log('MongoDB connection closed');
     } catch (error) {
         console.error('Error Connecting to MongoDB', error);
-        res.status(500).send({ message: `${req.body.fmsName} Step 1 is NOT Created`, status: 500 });
+        res.status(500).send({ message: ` Step 1 is NOT Created`, status: 500 });
     }
+});
+
+
+//edit fmsStep 1
+initialiseFms.post('/editFmsStep1', async (req, res) => {
+  console.log(" Edit Fms Step 1 API hit");
+  console.log(req.body);
+
+  // Initialize variables to hold user details
+  let userName = "";
+  let userID = "";
+  let companyUrl = "";
+  let userEmail = "";
+
+  console.log(req.headers.authorization)
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+      console.log("error: Authorization header missing or malformed");
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const token = authHeader.split(" ")[1];
+
+    console.log('token fetched is ' , token)
+
+  try {
+      // Fetch user details and company details based on the token
+      const response = await axios.post(process.env.MAIN_BE_URL, { token: token });
+      console.log('Fetched User Details and Company Details', response.data);
+      userName = response.data.emp_name;
+      userID = response.data.user_id;
+      companyUrl = response.data.verify_company_url;
+      userEmail = response.data.email_id;
+  } catch (error) {
+      console.error('Error posting data:', error);
+      res.status(500).send({ message: 'Error fetching user details', status: 500 });
+      return;
+  }
+
+  try {
+      // Connect to MongoDB and perform operations
+      const client = await MongoClient.connect(process.env.MONGO_DB_STRING);
+      console.log('Connected to database');
+      const db = client.db(companyUrl);
+      const collection = db.collection('fmsMaster');
+
+      
+      ////Fetch process details
+      const processDetailsResponse = await axios.post(process.env.MAIN_BE_PROCESS_URL, {
+        p_id: req.body.fmsProcess,
+        verify_company_url: companyUrl
+    });
+
+ //console.log( processDetailsResponse.data.result);
+
+ //const currentDate = moment().tz('Asia/Kolkata').format();
+
+      // Filter document to find the document to update
+    const filter = { fmsTaskId: taskId };
+
+    const update = {
+          $set: {
+              fmsName: req.body.fmsName,
+              fmsDescription: req.body.fmsDescription,
+              processDetailsResponse : processDetailsResponse
+          },
+        };
+    
+
+    //Perform the update operation
+    const result = await collection.updateOne(filter, update);
+
+      res.json({
+          "message": `${req.body.fmsName} Step 1 is Successfully Edited`,
+          "status": 200,
+          "data": result // Include the updated document in the response
+      });
+
+      console.log(result);
+
+      // Close the MongoDB connection
+      await client.close();
+      console.log('MongoDB connection closed');
+  } catch (error) {
+      console.error('Error Connecting to MongoDB', error);
+      res.status(500).send({ message: ` Step 1 is NOT Edited`, status: 500 });
+  }
 });
 
 
